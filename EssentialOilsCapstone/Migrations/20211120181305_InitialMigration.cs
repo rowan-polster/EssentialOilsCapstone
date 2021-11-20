46c6b050598294541942cdabe2426c21e8ea63ba
Rowan Polster <rowandownriver94@gmail.com>
2021-11-20 12:15:00 -0600

    Run successful migration

---
+++ EssentialOilsCapstone/Migrations/20211120181305_InitialMigration.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EssentialOilsCapstone.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EssentialOils",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EssentialOils", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Treatment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Treatment", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OilTreatment",
+                columns: table => new
+                {
+                    TreatmentId = table.Column<int>(nullable: false),
+                    OilId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OilTreatment", x => new { x.OilId, x.TreatmentId });
+                    table.ForeignKey(
+                        name: "FK_OilTreatment_EssentialOils_OilId",
+                        column: x => x.OilId,
+                        principalTable: "EssentialOils",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OilTreatment_Treatment_TreatmentId",
+                        column: x => x.TreatmentId,
+                        principalTable: "Treatment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OilTreatment_TreatmentId",
+                table: "OilTreatment",
+                column: "TreatmentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OilTreatment");
+
+            migrationBuilder.DropTable(
+                name: "EssentialOils");
+
+            migrationBuilder.DropTable(
+                name: "Treatment");
+        }
+    }
+}
