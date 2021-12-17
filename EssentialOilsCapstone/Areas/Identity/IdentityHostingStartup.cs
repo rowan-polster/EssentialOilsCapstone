5323afb642e000617e74d39d8a265f4e0b0b3be4
Megan Daubenmier <megan.daubenmier@gmail.com>
2021-12-16 21:06:18 -0600

    Stopping point for 12/16/2021.

---
+++ EssentialOilsCapstone/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EssentialOilsCapstone.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EssentialOilsCapstone.Areas.Identity.IdentityHostingStartup))]
+namespace EssentialOilsCapstone.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<OilDbContext>(options =>
+                 options.UseMySql(
+                     context.Configuration.GetConnectionString("DefaultConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                 .AddEntityFrameworkStores<OilDbContext>();
+            });
+        }
+    }
+}
