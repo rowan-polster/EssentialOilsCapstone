1c7ba565ecbb353e6ae839569232d28095458601
Megan Daubenmier <megan.daubenmier@gmail.com>
2022-01-17 13:27:29 -0600

    Stars are now connected to back end but not showing up as yellow.

--- EssentialOilsCapstone/ViewModels/OilDetailViewModel.cs
+++ EssentialOilsCapstone/ViewModels/OilDetailViewModel.cs
@@ -16,12 +16,30 @@ namespace EssentialOilsCapstone.ViewModels
         public string Description { get; set; }
         public string PropertyText { get; set; }
         public string UserNotes { get; set; }
-
+        public int NumStars { get; set; }
         public OilDetailViewModel(Oil theOil, List<OilProperty> oilProperties)
         {
             OilId = theOil.Id;
             Name = theOil.Name;
             Description = theOil.Description;
+
+            PropertyText = "";
+            for (int i = 0; i < oilProperties.Count; i++)
+            {
+                PropertyText += oilProperties[i].Property.Name;
+                if (i < oilProperties.Count - 1)
+                {
+                    PropertyText += ", ";
+                }
+            }
+        }
+
+        public OilDetailViewModel(Oil theOil, int numStars, List<OilProperty> oilProperties)
+        {
+            OilId = theOil.Id;
+            Name = theOil.Name;
+            Description = theOil.Description;
+            NumStars = numStars;
 
             PropertyText = "";
             for (int i = 0; i < oilProperties.Count; i++)
@@ -40,6 +58,25 @@ namespace EssentialOilsCapstone.ViewModels
             Name = theOil.Name;
             Description = theOil.Description;
             UserNotes = userNotes;
+
+            PropertyText = "";
+            for (int i = 0; i < oilProperties.Count; i++)
+            {
+                PropertyText += oilProperties[i].Property.Name;
+                if (i < oilProperties.Count - 1)
+                {
+                    PropertyText += ", ";
+                }
+            }
+        }
+
+        public OilDetailViewModel(Oil theOil, string userNotes, int numStars, List<OilProperty> oilProperties)
+        {
+            OilId = theOil.Id;
+            Name = theOil.Name;
+            Description = theOil.Description;
+            UserNotes = userNotes;
+            NumStars = numStars;
 
             PropertyText = "";
             for (int i = 0; i < oilProperties.Count; i++)
